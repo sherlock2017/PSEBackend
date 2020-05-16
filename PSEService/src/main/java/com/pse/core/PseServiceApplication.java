50a0d4aec667c018b5b20a9f4bfc0f174225ed77
Rishabh199520 <rishabh.mg02@gmail.com>
2020-05-17 01:59:33 +0530

    Project Setup

---
+++ PSEService/src/main/java/com/pse/core/PseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pse.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PseServiceApplication.class, args);
+	}
+
+}
